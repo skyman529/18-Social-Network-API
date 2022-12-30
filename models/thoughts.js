573a13cdb1c0ad7c5ab247fa22f568de4b2ef027
skyman529 <skyman529@gmail.com>
2022-12-30 08:35:35 -0700

    Update thoughts.js

--- models/thoughts.js
+++ models/thoughts.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const moment = require("moment");
 
 // Schema to create a thoughts model
 const reactionSchema = new Schema(
@@ -8,7 +9,7 @@ const reactionSchema = new Schema(
       type: String,
       required: true,
       minLength: 1,
-      maxLength: 250,
+      maxLength: 280,
     },
     userName: {
       type: String,
@@ -16,33 +17,47 @@ const reactionSchema = new Schema(
     createdAt: {
       type: Date,
       default: Date.now,
-      get: (time) => formatDate(time)
+      get: (date) => {
+        const formatDate = moment(date).format("MM/DD/YYYY");
+        return formatDate;  
     },
   },    
-  {
     toJSON: {
       virtuals: true,
     },
     id: false,
   }
 );
+
 const thoughtSchema = new Schema(
   {
     thoughtText: {
     type: String,
     required: true,
     minLength: 1,
-    maxLength: 250,
+    maxLength: 280,
   },
   createdAt: {
     type: Date,
     default: Date.now,
+    get: (date) => {
+        const formatDate = moment(date).format("MM/DD/YYYY");
+        return formatDate;
   },
+},
   username: {
     type: String,
+    required: true,
   },
   reactions: [reactionSchema],
-});
+},
+ {  
+    toJSON: {
+      getters: true,
+      virtuals: true,
+    },
+    id: false,
+  });
 
 thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
